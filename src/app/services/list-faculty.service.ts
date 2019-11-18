65f8ef43524873ca65d49003b49aee08771384c3
Andrés Largo <felipeflogxd@gmail.com>
2019-11-17 22:21:15 -0500

    Update list-faculty.service.ts

--- src/app/services/list-faculty.service.ts
+++ src/app/services/list-faculty.service.ts
@@ -8,31 +8,31 @@ export class ListFacultyService {
 
   constructor(private http: HttpClient) { }
   list = () => {
-    return this.http.get("http://localhost:10010/faculty/list") // pETICIÓN GET
+    return this.http.get("http://localhost:10060/faculty/list") // pETICIÓN GET
   }
   listLabs = () => {
-    return this.http.get("http://localhost:10010/lab/list") // pETICIÓN GET
+    return this.http.get("http://localhost:10060/lab/list") // pETICIÓN GET
   }
   listBuilding = () => {
-    return this.http.get("http://localhost:10010/building/list") // pETICIÓN GET
+    return this.http.get("http://localhost:10060/building/list") // pETICIÓN GET
   }
 
   save = (model) => {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return this.http.post("http://localhost:10010/building/add", model, { responseType: 'text'} )
+    return this.http.post("http://localhost:10060/building/add", model, { responseType: 'text'} )
   }
 
   saveFaculty = (model) => {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return this.http.post("http://localhost:10010/faculty/add", model, { responseType: 'text' })
+    return this.http.post("http://localhost:10060/faculty/add", model, { responseType: 'text' })
   }
 
   saveLab = (model) => {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json');
-    return this.http.post("http://localhost:10010/lab/add", model, { responseType: 'text' })
+    return this.http.post("http://localhost:10060/lab/add", model, { responseType: 'text' })
 
   }
 
